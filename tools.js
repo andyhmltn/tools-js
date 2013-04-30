a99b87bad6f878f1d132099b8ab636b83d146159
Andy Hamilton <andyhmltn@gmail.com>
2013-04-30 14:42:45 +0100

    Added a nifty `$` querySelector

--- tools.js
+++ tools.js
@@ -1,2 +1,5 @@
 Array.prototype.each = function(callback){for(i = 0; i < this.length; i++){callback(i, this[i]);}}
 Object.prototype.each = function(callback){Object.keys(this).each(function(i,v) { callback(i,this[v]) })}
+
+var $ = document.querySelectorAll.bind(document);
+Element.prototype.on = Element.prototype.addEventListener;
