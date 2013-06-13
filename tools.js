c94265e16dd7f0fe2c78b74f9007d385a5e04106
Andy Hamilton <andyhmltn@gmail.com>
2013-06-13 09:51:01 +0100

    Added comments to the script to make it more obvious

--- tools.js
+++ tools.js
@@ -1,5 +1,7 @@
+//Object/Array each methods
 Array.prototype.each = function(callback){for(i = 0; i < this.length; i++){callback(i, this[i]);}}
 Object.prototype.each = function(callback){Object.keys(this).each(function(i,v) { callback(i,this[v]) })}
 
+//jQuery-esque selector/bindings
 var $ = document.querySelectorAll.bind(document);
 Element.prototype.on = Element.prototype.addEventListener;
