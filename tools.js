2c8dd385c70df60250b28082e87c14fe2457cf75
Andy Hamilton <andyhmltn@gmail.com>
2013-11-06 11:21:38 +0000

    Made the code a lot more readable

--- tools.js
+++ tools.js
@@ -1,6 +1,15 @@
 //Object/Array each methods
-Array.prototype.each = function(callback){for(i = 0; i < this.length; i++){callback(i, this[i]);}}
-Object.prototype.each = function(callback){Object.keys(this).each(function(i,v) { callback(i,this[v]) })}
+Array.prototype.each = function(callback){
+  for(i = 0; i < this.length; i++){
+    callback(i, this[i]);
+  }
+}
+
+Object.prototype.each = function(callback){
+  Object.keys(this).each(function(i,v) { 
+    callback(i,this[v]) 
+  })
+}
 
 //jQuery-esque selector/bindings
 var $ = document.querySelectorAll.bind(document);
