1c3654f2cde2b19b62cf42e537a58502b1547744
Andy Hamilton <andyhmltn@gmail.com>
2013-11-06 11:26:20 +0000

    Added better comments

--- tools.js
+++ tools.js
@@ -1,19 +1,41 @@
-//Object/Array each methods
+// A function for cycling through arrays.
+// Example usage:
+//
+// [1,2,3].each(function(key, value) {
+//   console.log(key, value)
+// })
+
 Array.prototype.each = function(callback){
   for(i = 0; i < this.length; i++){
     callback(i, this[i]);
   }
 }
 
+// Similar to the above but for objects:
+// Example usage:
+//
+// {name: 'Andy', age: 999}.each(function(key, value) {
+//   console.log("%s: %s", key, value) #=> name: Andy / age: 999
+// })
+
 Object.prototype.each = function(callback){
-  Object.keys(this).each(function(i,v) { 
-    callback(i,this[v]) 
+  Object.keys(this).each(function(i,v) {
+    callback(i,this[v])
   })
 }
 
-//jQuery-esque selector/bindings
+// Easy DOM selection.
+// Example usage: $('body')
+
 var $ = document.querySelectorAll.bind(document);
 Element.prototype.on = Element.prototype.addEventListener;
+
+// Returns the type of a variable.
+// Example usage:
+//
+// type([])      #=> array
+// type({})      #=> object
+// type('hello') #=> string
 
 var type = function (o) {
     var s = Object.prototype.toString.call(o);
